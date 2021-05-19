e09f8b9a2e0a7ab485c7da4475d88a65978dac12
raghavendra95r <rrgowda95@gmail.com>
2021-05-19 23:37:17 +0530

    updated

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -3,14 +3,14 @@ import Towns from './Towns';
 
 
 const Cities = (props) => {
-    const [click ,setClick] = useState(false);
+    const [click1,setClick1] = useState(false);
     return (
         <>
-               <li onClick={()=> setClick(!click)}  id={`city${props.id}`}>
+               <li onClick={()=> setClick1(!click1)}  id={`city${props.id}`}>
                 <button>{props.city.name} </button>
             </li>
             <ul>
-                {click ? props.city.towns.map((town ,i) => {
+                {click1 ? props.city.towns.map((town ,i) => {
                     return (
                         <Towns town={town} key={i+1} id={i+1} />
                     )
